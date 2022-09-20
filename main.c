ec8adae19d634cd4fba1841462eaa2d52f023ab6
Olatundeawo <awotimilehin.babatunde>
2022-09-20 00:35:53 -0700

    added a program that tesded a program that test for size of type of variable

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* trying to follow betty style */
+
+/**
+ * main - Entry point
+ *
+ * Return: return
+ */
+
+	int main(void)
+{
+	int n;
+	 printf("size of type 'char' on my computer: %lu
+	bytes\n", sizeof(char));
+	 printf("size of type of my variable on my compu	ter: %lu bytes\n", sizeof(n));
+
+	 return (0);
+}
